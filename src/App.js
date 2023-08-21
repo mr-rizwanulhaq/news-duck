cc845190a45f2b2edc95c69f7ee73df2a21c5bd5
Muhammad Rizwan Ul Haq <rizaqua.6@gmail.com>
2023-08-21 18:55:23 +0500

    basename added

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
     return (
       <div>
 
-        <BrowserRouter>
+        <BrowserRouter basename='news-duck'>
         <NavBar></NavBar>
           <Routes>
               <Route exact path="/" element={<News pageSize={this.pageSize} newsCategory='Home'> </News>} />
